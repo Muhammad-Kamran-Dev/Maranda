e84dea2e5bcd98ec53b908dfd109a2171dae2102
Muhammad Kamran <kamran8915uop@gmail.com>
2023-07-25 20:24:19 +0500

    Navigation added

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+// Navigation Code
+const menu = document.querySelector(".menu-icon");
+const closeBtn = document.querySelector(".close-btn");
+const navList = document.querySelector("ul");
+
+menu.addEventListener("click", () => {
+  navList.classList.remove("close");
+  navList.classList.remove("hidden");
+});
+closeBtn.addEventListener("click", () => {
+  navList.classList.add("close");
+});
+
+// Gsap Animation Code
 // Target the .page-1 element
 const page1 = document.querySelector(".page-1");
 
@@ -11,16 +25,15 @@ tl.to(page1, {
   scale: 0.5
 });
 
-// Animation 2: Move the page back to the screen from the bottom and scale it down
+// Animation 2: Scale the page back to the screen from the bottom and scale it down
 tl.to(page1, {
   y: "0vh",
-  duration: 1,
+  delay: 0.2,
   scale: 0.5
 });
 
-// Animation 3: Scale the page up and rotate it
+// Animation 3:Scale the page  up and rotate it
 tl.to(page1, {
-  y: 0,
   duration: 1,
   rotation: -360,
   scale: 1
