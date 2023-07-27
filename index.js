361ebb5691b0f4bc46742ce34d4cf5dce7316815
Muhammad Kamran <kamran8915uop@gmail.com>
2023-07-27 13:15:32 +0500

    responsiveness added

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ closeBtn.addEventListener("click", () => {
 
 // Gsap Animation Code
 // Target the .page-1 element
-const page1 = document.querySelector(".page-1");
+const page1 = document.querySelector(".main");
 
 // Create a GSAP timeline
 const tl = gsap.timeline({ delay: 1 });
@@ -33,14 +33,16 @@ tl.to(page1, {
 
 // Animation 2: Scale the page back to the screen from the bottom and scale it down
 tl.to(page1, {
-  y: "0vh",
-  delay: 0.2,
-  scale: 0.5
+  y: "-150vh",
+  scale: 0.4,
+  duration: 1
 });
 
 // Animation 3:Scale the page  up and rotate it
 tl.to(page1, {
+  delay: 0.2,
   duration: 1,
   rotation: -360,
-  scale: 1
+  scale: 1,
+  y: 0
 });
